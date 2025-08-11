dfe8afbb6a8957d34fa42871a907d70498d2b7f2
pappicandy123 <pappicandy@gmail.com>
2025-08-12 00:54:21 +0100

    runtime changes

--- mike/settings.py
+++ mike/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1yy2+8j1!a@knk1ti)-y1l@+@ymwzycp!p9e$zt5$zd$hx)rum
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
